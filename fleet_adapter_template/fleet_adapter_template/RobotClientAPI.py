8ed00873e8374c61ca2629ae0bb838f8dcd6d2a9
Gary Bey <beyhy94@gmail.com>
2026-06-18 15:20:26 +0800

    Implemented minimal linting CI + adhered codebase to PEP257 & flake8 (#43)
    * :heavy_plus_sign: Added minimal CI for static analysis. Still failing. Yet to conform package to flake8 and pep257 fully.
    
    Signed-off-by: Gary Bey <beyhy94@gmail.com>
    
    * :hammer: Adhered package to flake8, copyright and pep257 standards with passing local CI.
    
    Signed-off-by: Gary Bey <beyhy94@gmail.com>
    
    * :hammer: Re-integrated back past 2 fixes concerning race condition execution and localisation hook.
    
    Signed-off-by: Gary Bey <beyhy94@gmail.com>
    
    * :hammer: Added back localise() in fleet_adapter.py.
    
    Signed-off-by: Gary Bey <beyhy94@gmail.com>
    
    * :hammer: Rectified wrong return value in is_command_completed function.
    
    Signed-off-by: Gary Bey <beyhy94@gmail.com>
    
    * :fire: Removed unused _get_map_name function in RobotClientAPI.py.
    
    Signed-off-by: Gary Bey <beyhy94@gmail.com>
    
    * :hammer: Rectified stop function in fleet_adapter.py such that it runs localised execution variable through conditions to prevent race condition.
    
    Signed-off-by: cardboardcode <beyhy94@gmail.com>
    
    * :hammer: Refactored docstring comments to all be consistently one-liner descriptions.
    
    Signed-off-by: cardboardcode <beyhy94@gmail.com>
    
    * :hammer: Introduced type-hinting in fleet_adapter.py for functions expected to be changed by end-users.
    
    Signed-off-by: cardboardcode <beyhy94@gmail.com>
    
    * :fire: Removed type-hinting for get_data function in RobotUpdateData to rectify failing CI.
    
    Signed-off-by: cardboardcode <beyhy94@gmail.com>
    
    * :fire: Removed repeated code.
    
    Signed-off-by: cardboardcode <beyhy94@gmail.com>
    
    * :hammer: Set type-hinting for position function call in RobotClientAPI.py more explicitly.
    
    Signed-off-by: Gary Bey <beyhy94@gmail.com>
    
    ---------
    
    Signed-off-by: Gary Bey <beyhy94@gmail.com>
    Signed-off-by: cardboardcode <beyhy94@gmail.com>

--- fleet_adapter_template/fleet_adapter_template/RobotClientAPI.py
+++ fleet_adapter_template/fleet_adapter_template/RobotClientAPI.py
@@ -13,13 +13,15 @@
 # limitations under the License.
 
 
-'''
-    The RobotAPI class is a wrapper for API calls to the robot. Here users
-    are expected to fill up the implementations of functions which will be used
-    by the RobotCommandHandle. For example, if your robot has a REST API, you
-    will need to make http request calls to the appropriate endpoints within
-    these functions.
-'''
+"""
+The RobotAPI class is a wrapper for API calls to the robot.
+
+Here users are expected to fill up the implementations of functions which
+will be used by the RobotCommandHandle. For example, if your robot has a
+REST API, you will need to make http request calls to the appropriate
+endpoints within these functions.
+
+"""
 
 
 class RobotAPI:
@@ -33,8 +35,8 @@ class RobotAPI:
         self.timeout = 5.0
         self.debug = False
 
-    def check_connection(self):
-        ''' Return True if connection to the robot API server is successful '''
+    def check_connection(self) -> bool:
+        """Return True if connection to the robot API server is successful."""
         # ------------------------ #
         # IMPLEMENT YOUR CODE HERE #
         # ------------------------ #
@@ -45,26 +47,21 @@ class RobotAPI:
         robot_name: str,
         pose,
         map_name: str,
-    ):
-        ''' Request the robot to localize on target map. This 
-            function should return True if the robot has accepted the 
-            request, else False '''
+    ) -> bool:
+        """Request the robot to localize on target map."""
         # ------------------------ #
         # IMPLEMENT YOUR CODE HERE #
         # ------------------------ #
         return False
-    
+
     def navigate(
         self,
         robot_name: str,
         pose,
         map_name: str,
         speed_limit=0.0
-    ):
-        ''' Request the robot to navigate to pose:[x,y,theta] where x, y and
-            and theta are in the robot's coordinate convention. This function
-            should return True if the robot has accepted the request,
-            else False '''
+    ) -> bool:
+        """Request the robot to navigate to pose:[x,y,theta]."""
         # ------------------------ #
         # IMPLEMENT YOUR CODE HERE #
         # ------------------------ #
@@ -75,78 +72,69 @@ class RobotAPI:
         robot_name: str,
         activity: str,
         label: str
-    ):
-        ''' Request the robot to begin a process. This is specific to the robot
-        and the use case. For example, load/unload a cart for Deliverybot
-        or begin cleaning a zone for a cleaning robot.
-        Return True if process has started/is queued successfully, else
-        return False '''
+    ) -> bool:
+        """Request the robot to begin a process."""
         # ------------------------ #
         # IMPLEMENT YOUR CODE HERE #
         # ------------------------ #
         return False
 
-    def stop(self, robot_name: str):
-        ''' Command the robot to stop.
-            Return True if robot has successfully stopped. Else False. '''
+    def stop(self, robot_name: str) -> bool:
+        """Request the robot to stop."""
         # ------------------------ #
         # IMPLEMENT YOUR CODE HERE #
         # ------------------------ #
         return False
 
-    def position(self, robot_name: str):
-        ''' Return [x, y, theta] expressed in the robot's coordinate frame or
-        None if any errors are encountered '''
+    def position(self, robot_name: str) -> tuple[float, float, float]:
+        """Return [x, y, theta] expressed in the robot's coordinate frame."""
         # ------------------------ #
         # IMPLEMENT YOUR CODE HERE #
         # ------------------------ #
         return None
 
-    def battery_soc(self, robot_name: str):
-        ''' Return the state of charge of the robot as a value between 0.0
-        and 1.0. Else return None if any errors are encountered. '''
+    def battery_soc(self, robot_name: str) -> float:
+        """Return state of charge of robot as a value between 0.0 and 1.0."""
         # ------------------------ #
         # IMPLEMENT YOUR CODE HERE #
         # ------------------------ #
         return None
 
-    def map(self, robot_name: str):
-        ''' Return the name of the map that the robot is currently on or
-        None if any errors are encountered. '''
+    def get_map_name(self, robot_name: str) -> str:
+        """Return the name of the map that the robot is currently on."""
         # ------------------------ #
         # IMPLEMENT YOUR CODE HERE #
         # ------------------------ #
         return None
 
-    def is_command_completed(self):
-        ''' Return True if the robot has completed its last command, else
-        return False. '''
+    def is_command_completed(self) -> bool:
+        """Return True if the robot has completed its last command."""
         # ------------------------ #
         # IMPLEMENT YOUR CODE HERE #
         # ------------------------ #
         return False
 
     def get_data(self, robot_name: str):
-        ''' Returns a RobotUpdateData for one robot if a name is given. Otherwise
-        return a list of RobotUpdateData for all robots. '''
-        map = self.map(robot_name)
+        """Return robot update data for the specified robot."""
+        map_name = self.get_map_name(robot_name)
         position = self.position(robot_name)
         battery_soc = self.battery_soc(robot_name)
-        if not (map is None or position is None or battery_soc is None):
-            return RobotUpdateData(robot_name, map, position, battery_soc)
+        if not (map_name is None or position is None or battery_soc is None):
+            return RobotUpdateData(robot_name, map_name, position, battery_soc)
         return None
 
 
 class RobotUpdateData:
-    ''' Update data for a single robot. '''
+    """Update data for a single robot."""
+
     def __init__(self,
                  robot_name: str,
-                 map: str,
+                 map_name: str,
                  position: list[float],
                  battery_soc: float,
                  requires_replan: bool | None = None):
         self.robot_name = robot_name
         self.position = position
-        self.map = map
+        self.map_name = map_name
         self.battery_soc = battery_soc
         self.requires_replan = requires_replan
